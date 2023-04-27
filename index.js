ada0e096b8bfd79688e9df170e5af82798acc582
BALLARD <mb0930877@dt035621.otc.edu>
2023-04-27 10:14:06 -0500

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 import chalk from 'chalk';
+import main from "./main.js";
+import {configs} from "./main.js";
+import axios from "axios"
+import {config} from "dotenv"
+config()
+console.log(process.env.API_KEY)
 
-console.log(chalk.blue('Hello world!'));
+main()
+configs();
+//console.log(chalk.blue('Hello world!'));
+let api_url = `https://dog.ceo/api/breeds/image/random?key=${process.env.API_KEY}`
+axios.get(api_url)
+.then(response =>{
+    // console.log(response.data);
+}) 
---
+++ main.js
@@ -0,0 +1,7 @@
+export default function ()
+{
+    console.log("I am the default function")
+}
+export function configs(){
+    console.log("I am the configs function");
+}
